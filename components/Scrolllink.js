ea438c3984e139669c43f5bd8247e9030d9e3dff
AstroPlaysBot <AstroPlays.help@gmail.com>
2026-02-03 23:04:29 +0100

    Update Scrolllink.js

--- components/Scrolllink.js
+++ components/Scrolllink.js
@@ -1,1 +1,15 @@
+export default function ScrollLink({ label, targetId }) {
+  const scroll = () => {
+    document.getElementById(targetId)?.scrollIntoView({ behavior: "smooth" });
+  };
+
+  return (
+    <button
+      onClick={scroll}
+      className="px-4 py-2 bg-gray-700 rounded hover:bg-gray-500"
+    >
+      {label}
+    </button>
+  );
+}
 
