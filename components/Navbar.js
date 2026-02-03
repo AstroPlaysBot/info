27d47e170a7b202e10432b46c180c0bb5e417575
AstroPlaysBot <AstroPlays.help@gmail.com>
2026-02-03 23:04:01 +0100

    Update Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,1 +1,9 @@
+export default function Navbar() {
+  return (
+    <nav className="fixed top-0 right-0 left-0 h-16 flex justify-end items-center px-5 bg-black/50 backdrop-blur-sm z-50">
+      <button className="mr-4 p-2 bg-blue-600 rounded">De/En</button>
+      <button className="p-2 bg-purple-600 rounded">Dashboard</button>
+    </nav>
+  );
+}
 
