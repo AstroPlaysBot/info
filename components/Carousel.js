c221e3ed924b02269473ebde8f1a34f02d11ba54
AstroPlaysBot <AstroPlays.help@gmail.com>
2026-02-06 13:58:32 +0100

    Update Carousel.js

--- components/Carousel.js
+++ components/Carousel.js
@@ -20,17 +20,17 @@ export default function Carousel() {
     Array.from({ length: 3 }, (_, i) => boxes[(start + i) % boxes.length]);
 
   return (
-    <div className="relative mt-10 flex items-center justify-center w-full max-w-5xl overflow-hidden">
+    <div className="relative mt-10 flex items-center justify-center w-full max-w-4xl overflow-hidden">
       {/* Left Button */}
       <button
         onClick={prev}
-        className="absolute left-0 bg-black/60 hover:bg-black/80 p-3 rounded-full z-10"
+        className="absolute left-0 bg-black/60 hover:bg-black/80 p-2 rounded-full z-10"
       >
         ◀
       </button>
 
       {/* Boxes */}
-      <div className="flex flex-nowrap items-center gap-5">
+      <div className="flex flex-nowrap items-center gap-3">
         {getVisible().map((box, index) => {
           const isCenter = index === 1;
 
@@ -38,20 +38,20 @@ export default function Carousel() {
             <div
               key={box.id}
               className={`
-                w-24 h-16 md:w-28 md:h-20
+                w-16 h-12 md:w-18 md:h-14
                 flex items-center justify-center
                 transition-all duration-500 ease-in-out
                 ${
                   isCenter
-                    ? "scale-110 opacity-100 z-10"
-                    : "scale-95 opacity-70"
+                    ? "scale-105 opacity-100 z-10"
+                    : "scale-95 opacity-60"
                 }
               `}
             >
               <img
                 src={box.image}
                 alt={`Box ${box.id}`}
-                className="w-full h-full object-contain drop-shadow-xl"
+                className="w-full h-full object-contain"
               />
             </div>
           );
@@ -61,7 +61,7 @@ export default function Carousel() {
       {/* Right Button */}
       <button
         onClick={next}
-        className="absolute right-0 bg-black/60 hover:bg-black/80 p-3 rounded-full z-10"
+        className="absolute right-0 bg-black/60 hover:bg-black/80 p-2 rounded-full z-10"
       >
         ▶
       </button>
